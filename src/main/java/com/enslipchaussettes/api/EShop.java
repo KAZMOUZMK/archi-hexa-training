35f19076f2822bea986cc42ae1f762f0f588db4b
Romain TOUZE <romain@rtodev.tech>
2023-09-29 15:05:13 +0200

    commit initial

---
+++ src/main/java/com/enslipchaussettes/api/EShop.java
@@ -0,0 +1,13 @@
+package com.enslipchaussettes.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EShop {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EShop.class, args);
+	}
+
+}
